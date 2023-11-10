118050befdd40e29dc841adeec7e186940e5bde4
jacewayland-125 <massimiliamomancini@gmail.com>
2023-11-10 11:46:17 +0100

    Changes

---
+++ C++/Es22.cpp
@@ -0,0 +1,11 @@
+/*Implementare un programma in C++, utiizzando Dev-C++
+in grado di effettuare la conversione di un numero
+intero da base 10 a base 2.*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+}
