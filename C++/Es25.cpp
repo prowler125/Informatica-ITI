d11da4beba84e06377834c5ab2705fb768369820
jacewayland-125 <144106569+jacewayland-125@users.noreply.github.com>
2023-12-06 16:54:44 +0100

    Changes

--- C++/Es25.cpp
+++ C++/Es25.cpp
@@ -5,5 +5,13 @@ using namespace std;
 
 int main()
 {
+    const int N = 10;
+    int vett[N];
+    cout << "Dammi i valori dell'Array da tastiera: " << endl;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> vett[i];
+    }
+
     return 0;
 }
