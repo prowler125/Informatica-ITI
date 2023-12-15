b71efd987da93ff1330d0470bd202c6f4b5869e1
jacewayland-125 <massimiliamomancini@gmail.com>
2023-12-15 09:48:52 +0100

    Changes

--- C++/Es34.cpp
+++ C++/Es34.cpp
@@ -2,6 +2,10 @@
 #include <cmath>
 using namespace std;
 
+void dadi(int a, int b, int c)
+{
+    
+}
 int main()
 {
     int a = 0, b = 0, c = 0;
---
+++ C++/Es35.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int inserimentoDati(int x = 0, int y = 0)
+{
+    cout << "Inserisci estremo inferiore intervallo: ";
+    cin >> x;
+    cout << "Inserisci estremo superiore intervallo: ";
+    cin >> y;
+    do
+    {
+        cout << "Inserisci estremo inferiore intervallo: ";
+        cin >> x;
+        cout << "Inserisci estremo superiore intervallo: ";
+        cin >> y;
+    } while (x != 0 || y != 0);
+    return 0;
+}
+int main()
+{
+    int x = 0, y = 0;
+    inserimentoDati(x, y);
+    return 0;
+}
