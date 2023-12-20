b3ca8ebdffed99faa5505c24cc160dd7882c7b17
jacewayland-125 <massimiliamomancini@gmail.com>
2023-12-20 11:36:40 +0100

    Changes

---
+++ C++/CASA/Es3.cpp
@@ -0,0 +1,27 @@
+/*Scrivere un programma che legga un array di interi di 10 posizioni e dica se in
+tutte le posizioni pari dell’array è memorizzato un numero pari*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    const int N = 10;
+    int vett[N];
+    for (int i = 0; i < N; i++)
+    {
+        cout << "Inserisci i valori dell'Array da tastiera: " << endl;
+        cin >> vett[i];
+        if (vett[i] % 2 == 0)
+        {
+            cout << "Il numero " << vett[i] << " è pari" << endl;
+        }
+        else
+        {
+            cout << "Il numero " << vett[i] << " è dispari" << endl;
+        }
+    }
+
+    return 0;
+}
