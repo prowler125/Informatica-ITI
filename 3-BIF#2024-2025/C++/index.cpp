e3cf135a8465c4261817dc1053311bea7b812f89
prowler125 <massimiliamomancini@gmail.com>
2024-09-20 08:34:25 +0200

    Add input and calculation logic for m and n values in index.cpp

--- 3-BIF#2024-2025/C++/index.cpp
+++ 3-BIF#2024-2025/C++/index.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    int m = 0;
+    int somma = m + n;
+    cout << "Dammi i valori di m e n: " << endl;
+    cin >> m;
+    cin >> n;
+}
