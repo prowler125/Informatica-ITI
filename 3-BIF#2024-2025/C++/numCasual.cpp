d644d400d6336747affc70aca0c7bb6c617f0a0c
prowler125 <massimiliamomancini@gmail.com>
2024-10-17 08:28:15 +0200

    Refactor numCasual.cpp to implement a number guessing game

---
+++ 3-BIF#2024-2025/C++/numCasual.cpp
@@ -0,0 +1,15 @@
+/*Ex10B
+
+Indovina un numero
+1. Genera un numero casuale X tra 1 e 1000
+2. Quindi chiedi di indovinare x dando come risposta
+se:
+    2.1 Troppo alto
+    2.2 Troppo basso
+3. Indovinato e dicendo in quanti tentativi  sono serviti*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+}
