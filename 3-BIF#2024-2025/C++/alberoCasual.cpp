97adda623450be0859ff3cfa9f66a378167719c1
prowler125 <massimiliamomancini@gmail.com>
2024-10-18 12:49:27 +0200

    Refactor alberoCasual.cpp to improve code structure and remove unused functions

--- 3-BIF#2024-2025/C++/alberoCasual.cpp
+++ 3-BIF#2024-2025/C++/alberoCasual.cpp
@@ -1,9 +1,9 @@
 /*Ex06 XF
-Letto un numero N (<=80), ed un carattere X, crea un
-ciclo che  stampi alternativamente (a mezzo di 2 for annidati):
+Letto un numero N (<=80), crea un ciclo che stampi
+alternativamente (a mezzo di 2 for annidati):
 
-    1.  Un TRIANGOLO Crescente (k da 1 a N) di X, dove per ogni riga k ci sono J  colonne (con j che va da 1 a k)
-    2.  Un TRIANGOLO Decrescente (k da N a 1) di X, dove per ogni riga k ci sono J colonne (con j che va da 1 a k)
+    1.  Un TRIANGOLO Crescente (k da 1 a N), dove per ogni riga k ci sono J  colonne (con j che va da 1 a k)
+    2.  Un TRIANGOLO Decrescente (k da N a 1), dove per ogni riga k ci sono J colonne (con j che va da 1 a k)
 
 Fare si che il for esterno sia in loop infinito
 Creare inoltre una funzione che stampa caratteri a caso*/
@@ -16,51 +16,41 @@ Creare inoltre una funzione che stampa caratteri a caso*/
 #include <cmath>
 using namespace std;
 
-int charCasual(int, char);
-int cresceTriang(int, char);
-int decresTriang(int, char);
+void printRandomCharacter()
+{
+    srand(time(NULL));
+    char randomChar = 'A' + rand() % 26;
+    cout << randomChar;
+}
+
 int main()
 {
     int N = 0;
     char X;
     cout << "Inserisca il valore di N: ";
     cin >> N;
-    cout << "Inserisca il carattere X: ";
-    cin >> X;
+    srand(time(NULL));
+    X = 'A' + rand() % 26;
 
     while (true)
     {
-        cresceTriang(N, X);
-        decresTriang(N, X);
+        for (int k = 1; k <= N; k++)
+        {
+            for (int j = 1; j <= k; j++)
+            {
+                printRandomCharacter();
+            }
+            cout << endl;
+        }
+
+        for (int k = N; k >= 1; k--)
+        {
+            for (int j = 1; j <= k; j++)
+            {
+                cout << X;
+            }
+            cout << endl;
+        }
     }
     return 0;
-}
-
-int charCasual(int N, char X)
-{
-    return 0;
-}
-
-int cresceTriang(int N, char X)
-{
-    for (int k = 1; k <= N; k++)
-    {
-        for (int j = 1; j <= k; j++)
-        {
-            cout << X;
-        }
-        cout << endl;
-    }
-}
-
-int decresTriang(int N, char X)
-{
-    for (int k = N; k >= 1; k--)
-    {
-        for (int j = 1; j <= k; j++)
-        {
-            cout << X;
-        }
-        cout << endl;
-    }
 }
