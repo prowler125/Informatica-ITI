c796ade3d8d61e46373f7a61746564a3d7846b9e
prowler125 <massimiliamomancini@gmail.com>
2024-11-08 12:14:06 +0100

    chore: Refactor C++ files to improve code structure and remove unused functions

---
+++ 3-BIF#2024-2025/C++/cornice.cpp
@@ -0,0 +1,19 @@
+/*Definire una funzione QCORNICE
+che ha per parametri un numero
+N ed un carattere X, che stampa
+solo cornice esterna di un
+quadrato di lato N fatto di
+caratteri X.
+
+Chiamarla nel main su numeri che
+vanno da 1 ad N con N letto da tastiera*/
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+using namespace std;
+
+void qCornice();
+int main(){
+
+}
