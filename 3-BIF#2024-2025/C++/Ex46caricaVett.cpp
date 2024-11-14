d9b98c2974f476dcd0cd997eb3ba0c87911aed8e
prowler125 <massimiliamomancini@gmail.com>
2024-11-14 09:08:27 +0100

    chore: Refactor Ex46caricaVett.cpp to improve code structure and add input validation for array elements

--- 3-BIF#2024-2025/C++/Ex46caricaVett.cpp
+++ 3-BIF#2024-2025/C++/Ex46caricaVett.cpp
@@ -3,3 +3,27 @@ Dichiara un vettore V  di N elementi interi.
 Caricalo con numeri (da tastiera, random
 oppure con indice  di un ciclo for);
 stampa il vettore*/
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+using namespace std;
+
+int main(){
+    const int N=20;
+    int vett[N];
+    cout << "Dammi il Valore dell'indice: 0"<< endl;
+    for (int i = 0; i <= N; i++)
+    {
+        cout<<"Indice: "<<i<<endl;
+        do
+        {
+            cin>>vett[i];
+            cout<<"Il Valore dell'Indice: "<<i<<" non è corretto, poiché minore di 0."<<endl;
+            cout<<"Reinserire il Valore, da tastiera."<<endl;
+        } while (vett[i]!=0 && vett[i]>0);
+        
+        cin>>
+    }
+    return 0;
+}
