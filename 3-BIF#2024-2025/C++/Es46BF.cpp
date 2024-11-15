66cd5d123889b439ea0edc25772353c762b51101
prowler125 <massimiliamomancini@gmail.com>
2024-11-15 12:46:45 +0100

    Refactor Es46BF.cpp to improve code structure and add input validation for array elements

---
+++ 3-BIF#2024-2025/C++/Es46BF.cpp
@@ -0,0 +1,41 @@
+/*Ex 46B-F
+Somma   vettore
+Dichiara una funzione sommaVet che ha come parametri un vettore 
+V di N elementi la sua dimensione N. Infine carica il vettore
+(caricaVet) e stampalo (stampaVet) poi stampa la somma*/
+
+#include <iostream>
+using namespace std;
+
+void stampaVett(int[], int);
+void caricaVett(int[], int);
+void sommaVett(int[], int);
+int main()
+{
+    const int N = 10;
+    int vett[N];
+    cout << "Dammi i valori del Vettore, da tastiera: " << endl;
+    for (int i = 0; i < N; i++)
+    {
+        vett[i] = i * 5;
+    }
+    stampaVett(vett, N);
+    return 0;
+}
+
+void stampaVett(int vett[], int N)
+{
+    cout << "Ecco il Vettore: ";
+    for (int i = 0; i < N; i++)
+    {
+        cout << vett[i] << "\t";
+    }
+}
+
+void caricaVett(int vett[], int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        vett[i] = i * 5;
+    }
+}
