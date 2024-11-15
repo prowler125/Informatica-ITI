276422a146418da48580f32dbac1b2cbbd37b95e
prowler125 <massimiliamomancini@gmail.com>
2024-11-15 12:16:59 +0100

    chore: Add 'array' header to C++ project settings and fix syntax error in Es46F.cpp

---
+++ 3-BIF#2024-2025/C++/Es46F.cpp
@@ -0,0 +1,31 @@
+/*Ex 46-F
+
+Stampa vettore
+
+Dichiara una funzione che ha come parametri un
+vettore V di N elementi e la sua dimensione N.
+Infine stampa il vettore*/
+
+#include <iostream>
+using namespace std;
+
+int stampaVett(const int);
+int caricaVett(int, int);
+int main()
+{
+
+    return 0;
+}
+
+int stampaVett(const int N = 10)
+{
+}
+int caricaVett(const int N = 10);
+{
+    int vett[N];
+    cout << "Dammi i valori del Vettore, da tastiera: " << endl;
+    for (int i = 0; i <= N; i++)
+    {
+        cin >> vett[i];
+    }
+}
