3a08050ab69f32579ee1c1b24bc86c651a7129ae
Prowler 125 <massimiliamomancini@gmail.com>
2024-11-18 16:20:43 +0100

    Add Ex46CF.cpp to implement a function for calculating the sum and average of an array

---
+++ 3-BIF#2024-2025/C++/Ex46CF.cpp
@@ -0,0 +1,14 @@
+/*
+Ex. 46C-F
+Somma e Media Vettore
+Dichiara e definisci una funzione max ed una media
+che hanno come parametri un vettore Vett di N elementi
+la sua dimensione N.
+Nel main usa le varie funzioni poi stampa la media e il massimo del Vettore
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+}
--- 3-BIF#2024-2025/C++/sommaVett.cpp
+++
@@ -1,5 +0,0 @@
-/*Ex 46B-F
-Somma Vettore
-Dichiara una funzione sommaVett che ha
-come parametri un vettore V di N elementi
-e ha come dimensione N. Carica il vettore (caricaVett) e stampalo (stampaVett)*/
