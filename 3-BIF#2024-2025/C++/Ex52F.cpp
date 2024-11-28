04f85a527b8cc6e2fd35deff65d084c688133084
prowler125 <massimiliamomancini@gmail.com>
2024-11-28 09:01:53 +0100

    feat: Implement functions for loading, multiplying, and summing vectors

---
+++ 3-BIF#2024-2025/C++/Ex52F.cpp
@@ -0,0 +1,46 @@
+/*
+EX52-F
+
+Vettore somma e moltiplica
+
+Scrivi  le seguenti due funzioni ognuna
+con 4 parametri (3 vettori lunghi N) dove
+letti i primi due V1 e V2 scrivono
+sul terzo V3 rispettivamente
+*/
+
+#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <string>
+#include <random>
+using namespace std;
+
+void caricaVett(int[], int);
+void molVett(int[], int[], int);
+void sommaVett(int[], int[], int);
+
+int main()
+{
+    const int N = 10;
+    int vett1[N];
+    int vett2[N];
+    int vett3[N];
+    return 0;
+}
+
+void caricaVett(int vett1[], int N)
+{
+    N = 10;
+    cout << "Dammi gli elementi del Vettore, da tastiera: " << endl;
+    for (int i = 0; i <= N; i++)
+    {
+        
+    }
+}
+void molVett(int vett1[], int vett2[], int N)
+{
+}
+void sommaVett(int vett1[], int vett2[], int)
+{
+}
