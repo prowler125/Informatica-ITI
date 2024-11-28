81f8a8951d29e5d8093d1f7843224048f4be01e1
prowler125 <massimiliamomancini@gmail.com>
2024-11-28 09:12:49 +0100

    Refactor C++ code to improve structure and add input validation for array elements

--- 3-BIF#2024-2025/C++/Ex52F.cpp
+++ 3-BIF#2024-2025/C++/Ex52F.cpp
@@ -11,36 +11,105 @@ sul terzo V3 rispettivamente
 
 #include <iostream>
 #include <math.h>
-#include <cmath>
-#include <string>
-#include <random>
 using namespace std;
+// prototipi
 
-void caricaVett(int[], int);
-void molVett(int[], int[], int);
-void sommaVett(int[], int[], int);
+void carVet(int[], int);
+void staVet(int[], int);
+int sommaVet(int[], int);
+int maxVet(int[], int);
+float mediaVet(int[], int);
+void sommaVettori(int[], int[], int[], int);
+int moltiplicaVettori(int[], int[], int[], int);
 
 int main()
 {
-    const int N = 10;
-    int vett1[N];
-    int vett2[N];
-    int vett3[N];
+    const int N = 8;
+    int v1[N];
+    int v2[N];
+    int v3[N];
+
+    // chiamate funzioni
+
+    staVet(v1, N);
+    carVet(v1, N);
+    staVet(v1, N);
+
+    staVet(v2, N);
+    carVet(v2, N);
+    staVet(v2, N);
+
+    sommaVettori(v1, v2, v3, N);
+    staVet(v3, N);
+
+    moltiplicaVettori(v1, v2, v3, N);
+    staVet(v3, N);
+
+    cout << "Somma = " << sommaVet(v3, N) << endl;
+    cout << "Massimo = " << maxVet(v3, N) << endl;
+    cout << "Prodotto =" << moltiplicaVettori(v1, v2, v3, N) << endl;
     return 0;
 }
 
-void caricaVett(int vett1[], int N)
+// carica vettore
+
+void carVet(int pvet[], int N)
 {
-    N = 10;
-    cout << "Dammi gli elementi del Vettore, da tastiera: " << endl;
-    for (int i = 0; i <= N; i++)
-    {
-        
-    }
+    for (int i = 0; i < N; i++)
+        pvet[i] = i + 1;
 }
-void molVett(int vett1[], int vett2[], int N)
+
+// stampa vettore
+
+void staVet(int pvet[], int N)
 {
+    for (int i = 0; i < N; i++)
+        cout << pvet[i] << "\t";
+    cout << endl;
 }
-void sommaVett(int vett1[], int vett2[], int)
+
+/// somma vettore
+
+int sommaVet(int pvet[], int N)
 {
+    int somma = 0;
+    for (int i = 0; i < N; i++)
+        somma = somma + pvet[i];
+    return somma;
+}
+
+// max vet
+
+int maxVet(int pvet[], int N)
+{
+    int pos, max = 0;
+    for (int i = 0; i < N; i++)
+        if (pvet[i] > max)
+            max = pvet[i];
+    return max;
+}
+
+// media vet
+
+float mediaVet(int pvet[], int N)
+{
+    float media = 0;
+    media = (float)sommaVet(pvet, N) / N;
+    return media;
+}
+
+// somma vettori
+
+void sommaVettori(int v1[], int v2[], int v3[], int N)
+{
+    for (int i = 0; i < N; i++)
+        v3[i] = v1[i] + v2[i];
+}
+
+// moltiplica vettori
+
+int moltiplicaVettori(int v1[], int v2[], int v3[], int N)
+{
+    for (int i = 0; i < N; i++)
+        v3[i] = v1[i] * v2[i];
 }
