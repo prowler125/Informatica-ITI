6b76d8bfd8554b7b0097df831808124265941d78
prowler125 <massimiliamomancini@gmail.com>
2024-12-05 08:45:32 +0100

    chore: Refactor C++ code to improve structure and add input validation for array elements

---
+++ 3-BIF#2024-2025/C++/Ex62-F.cpp
@@ -0,0 +1,54 @@
+/*Ex 62-F
+
+VETTORE ORDINA  (Exchange)
+
+Definisci una funzione ORDINAVETT che ordina
+un vettore in modo crescente;
+Allo scopo ORDINAVETT usa una funzione SCAMBIAVETT.
+    1. Nel main, dopo le opportune dichiarazioni procedi con:
+        1.2 Carica il vettore       CARICAVET
+        1.3 Stampa il vettore       STAMPAVET
+        1.4 Ordina il vettore in modo crescente      ORDINAVET e SCAMBIA
+        1.5 Stampa il vettore ordinato      STAMPAVET
+*/
+
+#include <math.h>
+#include <string>
+#include <time.h>
+#include <stdlib.h>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+void caricaVett();
+int stampaVett();
+void scambiaVett();
+int stampaVettOrd();
+
+int main()
+{
+    const int N = 10;
+    int vett[N];
+
+    for (int i = 0; i <= N; i++)
+    {
+        vett[i] = 0;
+    }
+
+    return 0;
+}
+
+void caricaVett()
+{
+    
+}
+int stampaVett()
+{
+}
+void scambiaVett()
+{
+}
+int stampaVettOrd()
+{
+}
