658ba7595da5fa461c5a72eaacb820a2c1a8f696
prowler125 <massimiliamomancini@gmail.com>
2024-12-05 09:38:54 +0100

    Refactor C++ code to improve structure and add input validation for array elements

--- 3-BIF#2024-2025/C++/ex52F_somma.cpp
+++ 3-BIF#2024-2025/C++/ex52F_somma.cpp
@@ -1,4 +1,4 @@
-// Ex52-F  somma di 2 vettori
+// Ex52-F	Somma di 2 vettori
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
@@ -7,55 +7,55 @@
 
 using namespace std;
 // prototipi
-void caricaVet  ( int [],int);
-void  stampaVet ( int [],int);
-void  sommaVet ( int  [], int[],int [],int);
-void  molVet ( int  [], int[],int [],int);
+void caricaVet(int[], int);
+void stampaVet(int[], int);
+void sommaVet(int[], int[], int[], int);
+void molVet(int[], int[], int[], int);
 int main()
-	{
+{
 	const int N = 6;
-	int v1[N] = { 100,101,102,103,104,105};
-	int v2[N], v3[N],k;
+	int v1[N] = {100, 101, 102, 103, 104, 105};
+	int v2[N], v3[N], k;
 	srand(time(NULL));
-	// carico   
-	caricaVet ( v1,N);
- 	caricaVet ( v2,N); 
+	// carico
+	caricaVet(v1, N);
+	caricaVet(v2, N);
 	// stampa i vettori
-	stampaVet ( v1,N);
-	stampaVet ( v2,N); 
-	   
+	stampaVet(v1, N);
+	stampaVet(v2, N);
+
 	// sommo i due vettori
-	sommaVet (v1,v2,v3,N);
-	cout<< endl;
-	stampaVet ( v3,N);
-	molVet (v1,v2,v3,N);
-	cout<< endl;
-	stampaVet ( v3,N);
+	sommaVet(v1, v2, v3, N);
+	cout << endl;
+	stampaVet(v3, N);
+	molVet(v1, v2, v3, N);
+	cout << endl;
+	stampaVet(v3, N);
 	return 0;
-	}
+}
 
 // funzione  carica vettore
-void caricaVet ( int vet [],int L)
-	{
-	for (int k=0; k< L; k++)
-			vet [ k ]= rand() %10 +1;
-	}
+void caricaVet(int vet[], int L)
+{
+	for (int k = 0; k < L; k++)
+		vet[k] = rand() % 10 + 1;
+}
 // funzione stampa vettore
-void  stampaVet ( int  vet [],int L)
-	{
-	for ( int k=0; k< L; k++)
-		cout <<  vet [ k ] << "\t";
- 	cout << endl;
-	}
-// funzione somma vettori	
-void  sommaVet ( int a[],int b[],int c[], int L )
-	{
-	for ( int  k=0; k< L ; k++)
-		c[k]= a[k]+b[k];
-	}
-	// funzione moltiplica 2 vettori	
-void  molVet ( int a[],int b[],int c[], int L )
-	{
-	for ( int  k=0; k< L ; k++)
-		c[k]= a[k]*b[k];
-	}
+void stampaVet(int vet[], int L)
+{
+	for (int k = 0; k < L; k++)
+		cout << vet[k] << "\t";
+	cout << endl;
+}
+// funzione somma vettori
+void sommaVet(int a[], int b[], int c[], int L)
+{
+	for (int k = 0; k < L; k++)
+		c[k] = a[k] + b[k];
+}
+// funzione moltiplica 2 vettori
+void molVet(int a[], int b[], int c[], int L)
+{
+	for (int k = 0; k < L; k++)
+		c[k] = a[k] * b[k];
+}
