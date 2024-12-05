ea6c6544c07f1ded9187296562c596509ba92019
prowler125 <massimiliamomancini@gmail.com>
2024-12-05 09:43:56 +0100

    Refactor C++ code to improve structure and add input validation for array elements

--- 3-BIF#2024-2025/C++/Ex52D-F.cpp
+++ 3-BIF#2024-2025/C++/Ex52D-F.cpp
@@ -9,13 +9,14 @@ using namespace std;
 // prototipi
 void caricaVet(int[], int);
 void stampaVet(int[], int);
-void sommaVet(int[], int[], int[], int);
+void sommaVet(int[], int[], int);
 void molVet(int[], int[], int[], int);
 int main()
 {
 	const int N = 6;
 	int v1[N] = {100, 101, 102, 103, 104, 105};
-	int v2[N], v3[N], k;
+	int v2[N], k;
+	int *v3;
 	srand(time(NULL));
 	// carico
 	caricaVet(v1, N);
@@ -25,10 +26,10 @@ int main()
 	stampaVet(v2, N);
 
 	// sommo i due vettori
-	sommaVet(v1, v2, v3, N);
+	sommaVet(v1, v2, N);
 	cout << endl;
 	stampaVet(v3, N);
-	molVet(v1, v2, v3, N);
+
 	cout << endl;
 	stampaVet(v3, N);
 	return 0;
@@ -48,14 +49,9 @@ void stampaVet(int vet[], int L)
 	cout << endl;
 }
 // funzione somma vettori
-void sommaVet(int a[], int b[], int c[], int L)
+void sommaVet(int a[], int b[], int L)
 {
+	int c[L];
 	for (int k = 0; k < L; k++)
 		c[k] = a[k] + b[k];
-}
-// funzione moltiplica 2 vettori
-void molVet(int a[], int b[], int c[], int L)
-{
-	for (int k = 0; k < L; k++)
-		c[k] = a[k] * b[k];
 }
