302cdd8ce47d1b83480eab8d68d2a03ec14d7484
prowler125 <massimiliamomancini@gmail.com>
2024-12-12 09:22:03 +0100

    Refactor C++ code to improve structure and add input validation for array elements

---
+++ 3-BIF#2024-2025/C++/Es58.cpp
@@ -0,0 +1,30 @@
+/*Ex 58M-F
+
+VETTORE CARATTERI INVERTI
+
+Definisci una funzione Reverse che inverte un vettore
+di caratteri e lo rende al chiamante in modo dinamico
+
+    1. Nel main,  dichiara un vettore CHAR V1[N];
+    2. Stampa v1 (STAMPAVET)
+    3. Carica v1 con lettere a caso (CARICAVET)
+
+Chiama la funzione REVERSE che legge v1 e lo lascia come è, ma
+restituisce  un  vettore  invertito.
+
+Infine stampa il vettore invertito  ( STAMPAVET)
+*/
+
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <iostream>
+using namespace std;
+
+void stampaVett();
+void caricaVett();
+
+int main()
+{
+}
