7779782ebf416ccf63648103e4b2c1531dd60d80
prowler125 <massimiliamomancini@gmail.com>
2024-12-13 12:17:08 +0100

    chore: Refactor C++ code to improve structure and add input validation for array elements

---
+++ 3-BIF#2024-2025/C++/Funzioni & PseudoCode/Ex.66B-F.cpp
@@ -0,0 +1,11 @@
+/* Ex66-F
+
+Matrice carica e stampa
+Scrivi un programma   che
+    1. Dichiara una matrice di interi   (R X C)
+    2. Poi definisci 2 funzioni:
+        2.1 StampaMat:  stampa matrice per righe;
+        2.2 CaricaMat: carica la matrice di numeri a caso
+        2.3 StaDiag1 e staDiag2: per stampare diagonale 1 e diagonale 2
+Nel main: stampa la matrice, caricala, stampala di nuovo
+*/
