9c03b2afac57236068caf8f46b1e5b41f24344c4
Prowler 125 <massimiliamomancini@gmail.com>
2024-12-18 15:10:48 +0100

    fix: Update comment formatting and enhance description for Fibonacci recursive function

--- 3-BIF#2024-2025/C++/Funzioni & PseudoCode/Ex08-R.cpp
+++ 3-BIF#2024-2025/C++/Funzioni & PseudoCode/Ex08-R.cpp
@@ -1,1 +1,30 @@
-/*Ex08-R Fibonacci : come  ex08-F, ma con funzione ricorsiva*/
+/*Ex08-R    Fibonacci
+Creare la sequenza di Fibonacci con una funzione ricorsiva
+*/
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+using namespace std;
+
+int funzFibo(int, int);
+
+int main()
+{
+    funzFibo();
+    return 0;
+}
+int funzFibo(int fn1, int fn2)
+{
+    int numTot = 0;
+    cout << "Con quanti numeri vuoi fare il calcolo? " << endl;
+    cin >> numTot;
+    for (int i = 0; i <= numTot; i++)
+    {
+        cout << fn2 << " ";
+        int ris = fn2 + fn1;
+        fn2 = fn1;
+        fn1 = ris;
+    }
+    return 0;
+}
