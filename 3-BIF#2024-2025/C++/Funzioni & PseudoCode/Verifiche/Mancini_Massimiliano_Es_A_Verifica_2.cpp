427d29d302daa8de0f51af9f9cc12c607f895702
prowler125 <massimiliamomancini@gmail.com>
2024-12-19 09:53:25 +0100

    Changes

---
+++ 3-BIF#2024-2025/C++/Funzioni & PseudoCode/Verifiche/Mancini_Massimiliano_Es_A_Verifica_2.cpp
@@ -0,0 +1,56 @@
+/*
+
+2° Verifica di Informatica
+
+Ex A
+Massimiliano Mancini
+19-12-2024
+
+*/
+
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <iostream>
+using namespace std;
+
+//Definizione di 2 variabili per il massimo di righe e colonne
+#define Max_C 20
+#define Max_R 20
+
+void carMat();
+void staMat();
+int somRighe();
+void staVett();
+void ordVett();
+void scambia();
+
+int main()
+{
+    return 0;
+}
+
+void carMat()
+{
+}
+
+void staMat()
+{
+}
+
+int somRighe()
+{
+}
+
+void staVett()
+{
+}
+
+void ordVett()
+{
+}
+
+void scambia()
+{
+}
