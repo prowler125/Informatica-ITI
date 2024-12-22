f442a9d2473466819bcb2c508bfbfbe1de276881
Prowler 125 <massimiliamomancini@gmail.com>
2024-12-22 13:01:26 +0100

    refactor: Update VSCode settings for C++ file associations and add new verification exercises

---
+++ 3-BIF#2024-2025/C++/Verifiche/Mancini_Massimiliano_Es_A_Verifica_2(Riprova).cpp
@@ -0,0 +1,35 @@
+/*
+Verifica di Informatica: riprova a casa
+Mancini Massimiliano
+
+Ex. A:
+
+Funzioni ed Array necessari:
+    1. Funzioni:
+        1.1 carMat: carica una matrice M x N di interi con numeri casuali compresi tra -10 e 10;
+        1.2 staMat:  stampa la  una matrice MAT di interi;
+        1.3 somRighe: ha in ingresso una matrice di interi M x N e restituisce
+            un vettore (lungo M) dove alla posizione k-esima ho la somma dei
+            numeri positivi alla riga k-esima della matrice;
+        1.4 staVet: stampa un vettore di interi;
+        1.5 ordVet: ordina un vettore in modo crescente;
+        1.6 scambia: inverte tra loro 2 numeri;
+
+
+Nel MAIN() dichiarare una matrice 20 x 20 a mezzo di define, leggere il numero
+di righe e colonne su cui si vuole lavorare (provare con 5 x 4) quindi fare le
+dichiarazioni e chiamate necessarie per:
+    1. Caricare e stampare una matrice 5 x 4
+    2. Chiamare la funzione somRighe
+    3. Stampare il vettore ottenuto
+    4. Ordinare il vettore in modo crescente ( ordVet)
+    5. Stampare di nuovo il vettore
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
---
+++ 3-BIF#2024-2025/C++/Verifiche/Mancini_Massimiliano_Es_B_Verifica_2(Riprova).cpp
@@ -0,0 +1,18 @@
+/*
+
+Verifica di Informatica: riprova a casa
+Mancini Massimiliano
+
+Ex. B:
+
+Scrivere un programma C++ che legge un valore X in euro (due decimali),
+quindi scrive quante monete occorrono  da:
+1 €,  10 cent, 1 cent,  per avere tale importo X, considerando di usare sempre le monete di taglio maggiore.
+
+esempio:   INPUT  valore x: 7.45, allora ho in OUTPUT  le seguenti monete:
+moneta    e/c        nr
+1        euro        7
+10        cent        4
+1        cent        5
+NB:
+fare attenzione a     tipi dato,  Commenti, Nomi,  Indentazione  .*/
