63b32bdecfbdab293ba6f72f6b2eaf6b54170b3e
Prowler 125 <massimiliamomancini@gmail.com>
2024-12-24 08:33:55 +0100

    refactor: Improve clarity of C++ program description and enhance code structure

--- 3-BIF#2024-2025/C++/Verifiche/Mancini_Massimiliano_Es_B_Verifica_2(Riprova).cpp
+++ 3-BIF#2024-2025/C++/Verifiche/Mancini_Massimiliano_Es_B_Verifica_2(Riprova).cpp
@@ -6,13 +6,32 @@ Mancini Massimiliano
 Ex. B:
 
 Scrivere un programma C++ che legge un valore X in euro (due decimali),
-quindi scrive quante monete occorrono  da:
-1 €,  10 cent, 1 cent,  per avere tale importo X, considerando di usare sempre le monete di taglio maggiore.
+quindi scrive quante monete occorrono da:
 
-esempio:   INPUT  valore x: 7.45, allora ho in OUTPUT  le seguenti monete:
+                1 €,
+                0,10 cent,
+                0,01 cent,
+
+per avere tale importo X, considerando di usare sempre le monete di taglio maggiore.
+
+Esempio:
+INPUT valore X: 7.45, allora ho in OUTPUT le seguenti monete:
 moneta    e/c        nr
 1        euro        7
 10        cent        4
 1        cent        5
 NB:
-fare attenzione a     tipi dato,  Commenti, Nomi,  Indentazione  .*/
+fare attenzione a tipi dato, Commenti, Nomi, Indentazione.
+*/
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
