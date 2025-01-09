06179ccda77c8bf5343a35471c2053e477c55467
prowler125 <massimiliamomancini@gmail.com>
2025-01-09 09:20:52 +0100

    Refactor C++ code to fix syntax error and improve structure

--- 3-BIF#2024-2025/Es19.cpp
+++ 3-BIF#2024-2025/Es19.cpp
@@ -1,1 +1,33 @@
+/*Ex 19
+STRINGA: ricerca carattere
+Scrivi un programma che dopo avere letto:
+- una stringa parola
+- un carattere x
+Stampa quante volte il carattere x compare in parola
+*/
+
 #include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string p;
+    char x;
+    int i = 0;
+    cout << "Scrivi un frase qui sotto: " << endl;
+    getline(cin, p);
+    cout << "Immetti un carattere: " << endl;
+    cin >> x;
+    int N = p.length();
+    for (int k = 0; k < N; k++)
+    {
+        if (p[k] == x)
+        {
+            i++;
+        }
+    }
+    cout << i << endl;
+
+    return 0;
+}
