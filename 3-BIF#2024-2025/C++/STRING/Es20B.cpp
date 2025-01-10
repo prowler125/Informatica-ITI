8797385ff862742cffff4e0e8c14a1ac7fa986b3
prowler125 <massimiliamomancini@gmail.com>
2025-01-10 12:17:16 +0100

    refactor: Add string reversal functionality to C++ program

--- 3-BIF#2024-2025/C++/STRING/Es20B.cpp
+++ 3-BIF#2024-2025/C++/STRING/Es20B.cpp
@@ -11,3 +11,15 @@ S2=S2+s1[];
 #include <iostream>
 #include <string>
 using namespace std;
+int main()
+{
+    string s1 = "esempio";
+    string s2 = "";
+
+    for (int i = s1.length() - 1; i >= 0; i--)
+    {
+        s2 += s1[i];
+    }
+
+    cout << "S2: " << s2 << endl;
+}
