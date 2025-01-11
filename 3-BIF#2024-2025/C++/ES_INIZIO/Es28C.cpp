4d688ca9c86c1df36b9b6daf28aca53401cad8e5
Prowler 125 <massimiliamomancini@gmail.com>
2025-01-11 18:53:49 +0100

    feat: Implement countdown program in C++ that counts down from a positive integer

---
+++ 3-BIF#2024-2025/C++/ES_INIZIO/Es28C.cpp
@@ -0,0 +1,35 @@
+/*
+Ex 28C COUNTDOWN
+Scrivere un programma che, letta una cifra intera X positiva
+conta alla rovescia da X fino ad 1(scatto ad ogni secondo).
+Suggerimento:
+    1. Libreria  time
+    2. Funzione sleep (t)
+*/
+
+#include <iostream>
+#include <ctime>
+#include <unistd.h>
+using namespace std;
+
+int main()
+{
+    int X;
+    cout << "Inserisci un numero intero positivo: ";
+    cin >> X;
+
+    if (X > 0)
+    {
+        for (int i = X; i > 0; --i)
+        {
+            cout << i << endl;
+            sleep(1);
+        }
+    }
+    else
+    {
+        cout << "Il numero inserito non è positivo." << endl;
+    }
+
+    return 0;
+}
