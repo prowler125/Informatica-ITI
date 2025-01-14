397adb06b29df9899040d49219a8e3a52af99d4c
Prowler 125 <massimiliamomancini@gmail.com>
2025-01-14 19:40:38 +0100

    feat: Modifica le stampe nel file Es1.py per migliorare la formattazione dell'output

--- 3-BIF#2024-2025/Python/Es1.py
+++ 3-BIF#2024-2025/Python/Es1.py
@@ -9,10 +9,10 @@ B=12
 C='ciao'
 D=0,2345
 print("Questo è A: ")
-print("A", A)
+print("A =", A)
 print("Questo è B: ")
-print("B", B)
+print("B =", B)
 B=A+1
 print("Questo è B, dopo la modifica: ", B)
-print("C", C)
-print("D", D)
+print("C =", C)
+print("D =", D)
