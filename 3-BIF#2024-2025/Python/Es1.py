48832a239c9621e8a668fee73c9d824244ec2475
Prowler 125 <massimiliamomancini@gmail.com>
2025-01-14 19:39:33 +0100

    feat: Aggiungi nuove stampe e variabili nel primo esercizio in Python

--- 3-BIF#2024-2025/Python/Es1.py
+++ 3-BIF#2024-2025/Python/Es1.py
@@ -1,2 +1,18 @@
 #Primo Esercizio in Python
+print("Prima Parte: ")
 print(1, 2, 3, sep='|')
+A=10
+print(A)
+print("Seconda Parte: ")
+A=10
+B=12
+C='ciao'
+D=0,2345
+print("Questo è A: ")
+print("A", A)
+print("Questo è B: ")
+print("B", B)
+B=A+1
+print("Questo è B, dopo la modifica: ", B)
+print("C", C)
+print("D", D)
