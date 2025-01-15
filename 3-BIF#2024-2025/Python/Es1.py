9ea253f297c025044f7b1c5459d9aa720e1e6d32
Prowler 125 <massimiliamomancini@gmail.com>
2025-01-15 15:23:29 +0100

    feat: Aggiungi nuove sezioni e logica per la somma e la temperatura nel file Es1.py

--- 3-BIF#2024-2025/Python/Es1.py
+++ 3-BIF#2024-2025/Python/Es1.py
@@ -1,4 +1,5 @@
 #Primo Esercizio in Python
+"""
 print("Prima Parte: ")
 print(1, 2, 3, sep='|')
 A=10
@@ -16,4 +17,24 @@ B=A+1
 print("Questo è B, dopo la modifica: ", B)
 print("C =", C)
 print("D =", D)
-print()
+"""
+"""
+print("Terza Parte: ")
+nome=input("Come ti chiami? ")
+print("Ciao", nome)
+print("Quarta Parte: ")
+print("Somma Artmetica semplice")
+A=input("Dammi il primo Numero A: ")
+B=input("Dammi il primo Numero B: ")
+S=int(A)+int(B)
+print("Somma: ", S)
+"""
+print("Quinta Parte:")
+#Chiedo la temperatura all'Utente
+temperatura=int(input("Immetti la temperatura: "))
+if(temperatura>21):
+    print("Fa caldo!")
+elif(temperatura<10):
+    print("Fa freddo!")
+else:
+    print("Si sta bene")
