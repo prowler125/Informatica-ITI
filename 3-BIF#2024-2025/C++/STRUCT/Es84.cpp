4c97d9611236eb838febf920aea91bd63d73b301
prowler125 <massimiliamomancini@gmail.com>
2025-01-16 09:05:58 +0100

    feat: Add struct for telephone directory and implement basic functionality

---
+++ 3-BIF#2024-2025/C++/STRUCT/Es84.cpp
@@ -0,0 +1,11 @@
+/*Ex 84             RUBRICA  TELEFONICA
+
+Scrivi  un programma   che  gestisce, per mezzo di un  vettore di struct,  una rubrica telefonica di numeri  di telefono fissi e cellulari, cognome e  nome, usando il tipo dato struct Agenda con
+campi COG ,  NOME , FISSO,  CELL  .
+Nel Main:
+a.    Carica 3  voci di rubrica
+
+b.    stampa  tutta la rubrica
+
+c.   Letto un cognome da  tastiera, cerca ( se esiste)   la
+prima  voce con quel cognome  e quindi stampa tutti  i dati della voce di rubrica e anche il  numero di posizione ( se non esiste  -1)*/
