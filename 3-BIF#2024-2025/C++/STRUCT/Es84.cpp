e7835f39e7e91ba13d19e075778158cf8c0a9762
prowler125 <massimiliamomancini@gmail.com>
2025-01-16 09:29:23 +0100

    feat: Add vector header and implement telephone directory functionality

--- 3-BIF#2024-2025/C++/STRUCT/Es84.cpp
+++ 3-BIF#2024-2025/C++/STRUCT/Es84.cpp
@@ -15,6 +15,7 @@ Nel Main:
 
 #include <iostream>
 #include <string>
+#include <vector> // Add this line
 #include <math.h>
 #include <cmath>
 using namespace std;
@@ -29,10 +30,12 @@ struct Agenda
 
 int main()
 {
-    Agenda agenda;
+    vector<Agenda> rubrica;
     int N = 3;
-    while (N == 0)
+
+    for (int i = 0; i < N; i++)
     {
+        Agenda agenda;
         cout << "Dammi il Nome: " << endl;
         cin >> agenda.nome;
         cout << "Dammi il Congome: " << endl;
@@ -41,6 +44,33 @@ int main()
         cin >> agenda.numFisso;
         cout << "Inserisci il tuo prefisso per CELLULARE: " << endl;
         cin >> agenda.numCell;
+        rubrica.push_back(agenda);
+    }
+    string cognomeCercato;
+    cout << "Inserisci il cognome da cercare: " << endl;
+    cin >> cognomeCercato;
+
+    int posizione = -1;
+    for (int i = 0; i < rubrica.size(); i++)
+    {
+        if (rubrica[i].cognome == cognomeCercato)
+        {
+            posizione = i;
+            break;
+        }
+    }
+
+    if (posizione != -1)
+    {
+        cout << "Voce di rubrica trovata alla posizione " << posizione << ":" << endl;
+        cout << "Nome: " << rubrica[posizione].nome << endl;
+        cout << "Cognome: " << rubrica[posizione].cognome << endl;
+        cout << "Numero fisso: " << rubrica[posizione].numFisso << endl;
+        cout << "Numero cellulare: " << rubrica[posizione].numCell << endl;
+    }
+    else
+    {
+        cout << "Nessuna voce di rubrica trovata con il cognome inserito." << endl;
     }
     return 0;
 }
