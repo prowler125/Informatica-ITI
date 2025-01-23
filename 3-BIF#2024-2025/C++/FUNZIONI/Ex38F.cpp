e045a8f4fa88182856e9078de63f7385bc8fc24b
prowler125 <massimiliamomancini@gmail.com>
2025-01-23 09:48:30 +0100

    feat: Aggiungi programma per calcolare il fattoriale tramite funzione ricorsiva in Ex38F.cpp

---
+++ 3-BIF#2024-2025/C++/FUNZIONI/Ex38F.cpp
@@ -0,0 +1,25 @@
+/*
+Ex 38F Funzione Fattoriale
+Calcolo Fattoriale per mezzo di unafunzione
+Idem ex38, ma definendo una funzione FATTOR
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int FATTOR(int n) {
+    if (n <= 1) {
+        return 1;
+    } else {
+        return n * FATTOR(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    cout << "Inserisci un numero: ";
+    cin >> num;
+    cout << "Il fattoriale di " << num << " è " << FATTOR(num) << endl;
+    return 0;
+}
