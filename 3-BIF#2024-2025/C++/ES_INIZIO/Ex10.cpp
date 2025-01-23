304b3dbf229a7c3c0ae556a279bb17bff0558802
prowler125 <massimiliamomancini@gmail.com>
2025-01-23 09:56:55 +0100

    feat: Aggiungi programma per generare numeri casuali in Ex10.cpp

---
+++ 3-BIF#2024-2025/C++/ES_INIZIO/Ex10.cpp
@@ -0,0 +1,21 @@
+/*
+Ex10
+Numeri a caso
+Usare:
+    1. #include <time.h>
+    2. srand(time(NULL)
+    rand ()  ...;
+Genera un numero casuale X tra:
+
+
+-  1 e  6
+
+
+-  5 e 10
+
+
+- - 5 e 5
+
+
+- 1 ed N letto da tastiera
+*/
