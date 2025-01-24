58063e450d8ed1652623bfd487ebe2c93846cc4f
prowler125 <massimiliamomancini@gmail.com>
2025-01-24 12:14:19 +0100

    feat: Aggiungi programma per leggere parole da un file e stamparle in Es89.cpp

---
+++ 3-BIF#2024-2025/C++/FILE/Es89.cpp
@@ -0,0 +1,30 @@
+/*
+Ex89 File leggi parole
+Vedi libro pag 222
+Leggi parole una alla volta da un file dati.txt e stampale andando a capo ogni volta
+*/
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <math.h>
+#include <string>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    ifstream file("dati.txt");
+    if (!file.is_open()) {
+        cerr << "Errore nell'apertura del file" << endl;
+        return 1;
+    }
+
+    string parola;
+    while (file >> parola) {
+        cout << parola << endl;
+    }
+
+    file.close();
+    return 0;
+}
