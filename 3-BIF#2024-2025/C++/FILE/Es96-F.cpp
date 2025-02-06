9e2f0fb2b90f41ee6e311c13bf6f0cf61d8c1c16
prowler125 <massimiliamomancini@gmail.com>
2025-02-06 09:26:08 +0100

    feat: Add functions to copy file data by character and by line
    The code changes introduce two new functions, `copiaFileC` and `copiaFileR`, which copy data from one file to another. The `copiaFileC` function copies the data character by character, while the `copiaFileR` function copies the data line by line. These functions are implemented in the file `Es96-F.cpp`.
    
    This commit also includes the addition of three new text files: `in.txt`, `out.txt`, and `outR.txt`. These files will be used to test the functionality of the `copiaFileC` and `copiaFileR` functions.
    
    The purpose of this commit is to provide the functionality to copy file data using different methods and to prepare for testing the results.

---
+++ 3-BIF#2024-2025/C++/FILE/Es96-F.cpp
@@ -0,0 +1,9 @@
+/*
+Ex 96-F
+File: funzione Copia file
+Definisci una funzione copiaFileC che ha come parametri formali due oggetti file passati per riferimento:
+    1. F1 di ingresso;
+    2. F2 di uscita.
+La funzione copia per carattere i dati letti da F1 nel file F2.
+Poi definisci una funzione copiaFileR analoga alla precedente, ma che copia i dati per righe.
+Chiama nel main le due funzioni e verifica i risultati sui file fisici: in.txt, out.txt,  outR.txt .*/
