23ef512e529115b9a07be6e71ab754ed6248be0d
prowler125 <massimiliamomancini@gmail.com>
2025-02-06 09:29:36 +0100

    Add support for copying file data by character and by line
    The code changes introduce two new functions, `copiaFileC` and `copiaFileR`, which allow for copying data from one file to another. The `copiaFileC` function copies the data character by character, while the `copiaFileR` function copies the data line by line. These functions are implemented in the file `Es96-F.cpp`.
    
    This commit also includes the addition of three new text files: `in.txt`, `out.txt`, and `outR.txt`. These files will be used to test the functionality of the `copiaFileC` and `copiaFileR` functions.
    
    The purpose of this commit is to provide the functionality to copy file data using different methods and to prepare for testing the results.

--- 3-BIF#2024-2025/C++/FILE/Es96-F.cpp
+++ 3-BIF#2024-2025/C++/FILE/Es96-F.cpp
@@ -6,4 +6,55 @@ Definisci una funzione copiaFileC che ha come parametri formali due oggetti file
     2. F2 di uscita.
 La funzione copia per carattere i dati letti da F1 nel file F2.
 Poi definisci una funzione copiaFileR analoga alla precedente, ma che copia i dati per righe.
-Chiama nel main le due funzioni e verifica i risultati sui file fisici: in.txt, out.txt,  outR.txt .*/
+Chiama nel main le due funzioni e verifica i risultati sui file fisici: in.txt, Z://Informatica-ITI//3-BIF#2024-2025//C++//FILE//TXT//out.txt, Z://Informatica-ITI//3-BIF#2024-2025//C++//FILE//TXT//outR.txt.
+*/
+
+// Z://Informatica-ITI//3-BIF#2024-2025//C++//FILE//TXT//input.txt
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+void copiaFileC(ifstream &F1, ofstream &F2)
+{
+    char ch;
+    while (F1.get(ch))
+    {
+        F2.put(ch);
+    }
+}
+
+void copiaFileR(ifstream &F1, ofstream &F2)
+{
+    string line;
+    while (getline(F1, line))
+    {
+        F2 << line << endl;
+    }
+}
+
+int main()
+{
+    ifstream inputFile("Z://Informatica-ITI//3-BIF#2024-2025//C++//FILE//TXT//in.txt");
+    ofstream outputFile("Z://Informatica-ITI//3-BIF#2024-2025//C++//FILE//TXT//out.txt");
+    ofstream outputRFile("Z://Informatica-ITI//3-BIF#2024-2025//C++//FILE//TXT//outR.txt");
+
+    if (inputFile && outputFile && outputRFile)
+    {
+        copiaFileC(inputFile, outputFile);
+        inputFile.close();
+        outputFile.close();
+
+        inputFile.open("in.txt");
+        copiaFileR(inputFile, outputRFile);
+        inputFile.close();
+        outputRFile.close();
+
+        cout << "File copiato con successo." << endl;
+    }
+    else
+    {
+        cout << "Impossibile aprire i file." << endl;
+    }
+
+    return 0;
+}
