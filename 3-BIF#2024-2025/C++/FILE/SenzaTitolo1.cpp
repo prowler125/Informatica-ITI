14e7dd1f7ce2f677efd3a5c516d3d50ffb6fba36
prowler125 <massimiliamomancini@gmail.com>
2025-02-06 09:16:03 +0100

    chore: Update file Es93-C.cpp to count consecutive occurrences of a character

--- 3-BIF#2024-2025/C++/FILE/SenzaTitolo1.cpp
+++ 3-BIF#2024-2025/C++/FILE/SenzaTitolo1.cpp
@@ -6,14 +6,37 @@ un carattere digitato da tastiera dall’utente restituisca tramite la
 console quante volte quel carattere compare per due volte
 consecutive nel file “input.txt”.
 */
+
 #include <iostream>
 #include <fstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
 using namespace std;
+
+int contaCaratteriConsecutivi(const string &, char);
+const string FILE_PATH = "Z://Informatica-ITI//3-BIF#2024-2025//C++//FILE//TXT//input.txt";
+
+int main()
+{
+    string filename = "Z://Informatica-ITI//3-BIF#2024-2025//C++//FILE//TXT//input.txt";
+    char targetChar;
+
+    cout << "Inserisci un carattere: ";
+    cin >> targetChar;
+
+    int conteggioConsecutivo = contaCaratteriConsecutivi(filename, targetChar);
+    cout << "Il carattere '" << targetChar << "' appare consecutivamente "
+         << conteggioConsecutivo << " volte nel file." << endl;
+
+    return 0;
+}
 
 int contaCaratteriConsecutivi(const string &filename, char targetChar)
 {
     ifstream inputFile("Z://Informatica-ITI//3-BIF#2024-2025//C++//FILE//TXT//input.txt");
-    if (!inputFile)
+    if (inputFile.fail())
     {
         cerr << "Impossibile aprire il file." << endl;
         return -1;
@@ -34,20 +57,5 @@ int contaCaratteriConsecutivi(const string &filename, char targetChar)
 
     inputFile.close();
     return count;
-}
-
-int main()
-{
-    string filename = "input.txt";
-    char targetChar;
-
-    cout << "Inserisci un carattere: ";
-    cin >> targetChar;
-
-    int conteggioConsecutivo = contaCaratteriConsecutivi(filename, targetChar);
-    cout << "Il carattere '" << targetChar << "' appare consecutivamente "
-         << conteggioConsecutivo << " volte nel file." << endl;
-
-    return 0;
 }
 
