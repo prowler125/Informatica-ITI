57223a1deb4f18546d7031f6863f4b66e3bc3631
Prowler125 <massimiliamomancini@gmail.com>
2025-02-12 15:37:28 +0100

    feat: Aggiungi il secondo esercizio in Python con utilizzo di if, else ed elif nel file condizioni.py

--- 3-BIF#2024-2025/CorsoIA/TEORIAPYTHON/condizioni.py
+++ 3-BIF#2024-2025/CorsoIA/TEORIAPYTHON/condizioni.py
@@ -1,5 +1,13 @@
-'''Primo Esercizio in Python del Corso I.A.'''
+'''Secondo Esercizio in Python del Corso I.A.'''
 
 
-#Prima Parte: uso Sintassi Base
-
+#Prima Parte: utilizzo di if, else ed elif
+print('Prima Parte: utilizzo di if, else ed elif')
+A=input("Immetti A: ")
+B=input("Immetti B: ")
+C=input("Immetti C: ")
+if(A>B):
+    if(A>C):
+        print(f'A = {A} è maggiore di B = {B} ed è maggiore anche di C = {C}')
+    else:
+        print(f'A = {A} è maggiore SOLO di B = {B}')
