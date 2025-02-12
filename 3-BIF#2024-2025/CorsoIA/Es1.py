5aa6e7e93158bce3192425bb5abf51fc6205eab5
Prowler125 <massimiliamomancini@gmail.com>
2025-02-12 15:07:53 +0100

    feat: Aggiungi pause e miglioramenti nella seconda parte del primo esercizio in Python

--- 3-BIF#2024-2025/CorsoIA/Es1.py
+++ 3-BIF#2024-2025/CorsoIA/Es1.py
@@ -1,13 +1,27 @@
+import os
+
 '''Primo Esercizio in Python del Corso I.A.'''
-#Prima Parte: uso Sintassi Base e primi tipi di Variabili
+
+#Prima Parte: uso Sintassi Base
+
 print('Prima Parte:')
 print('Utilizzo della Sintassi Base come il "print()"')
 print('Hello World!')
 print(1, 2, 3, sep='|')
 A=10
 print(A)
+os.system("pause")
+#Seconda Parte: primo uso delle Variabili
 print('Seconda Parte:')
+print('Primo uso delle Variabili')
+
+#Il tipo di Variabile in Python NON esiste: viene associato AUTOMATICAMENTE.
+print('Il tipo di Variabile in Python NON esiste: viene associato AUTOMATICAMENTE.')
 A=10
+print('Scritto sopra vi è, infatti, un esempio di Variabile intera: ', end="")
+print('A = {A}')
+print("Ecco il valore Intero di A:")
+print(A)
 B=12
 C='Ciao'
 D=0,2345
