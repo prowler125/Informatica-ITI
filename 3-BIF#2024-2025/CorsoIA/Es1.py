a24d308e497afd478cb13162c2906db2ab25bccd
Prowler125 <massimiliamomancini@gmail.com>
2025-02-12 15:20:20 +0100

    feat: Aggiungi la terza parte con input e operazioni aritmetiche nel primo esercizio in Python

--- 3-BIF#2024-2025/CorsoIA/Es1.py
+++ 3-BIF#2024-2025/CorsoIA/Es1.py
@@ -1,5 +1,6 @@
 '''Primo Esercizio in Python del Corso I.A.'''
 
+
 #Prima Parte: uso Sintassi Base
 
 print('Prima Parte:')
@@ -9,12 +10,14 @@ print(1, 2, 3, sep='|')
 A=10
 print(A)
 input("Premi un tasto per continuare...")
+
+
 #Seconda Parte: primo uso delle Variabili
+
 print('Seconda Parte:')
 print('Primo uso delle Variabili')
 
 #Il tipo di Variabile in Python NON esiste: viene associato AUTOMATICAMENTE.
-
 print('Il tipo di Variabile in Python NON esiste: viene associato AUTOMATICAMENTE.')
 
 A=10
@@ -28,3 +31,22 @@ print(f'C = {C}')
 D=0,2345
 print(f'Ancora, scritto sopra troviamo, un esempio di Float: ', end="")
 print(f'D = {D}')
+
+
+#Terza parte: uso di Input("") e operazioni Aritmetiche semplici
+
+print('Terza Parte')
+print('Uso di "input("")" e prime Operazioni Aritmetiche semplici')
+
+#Uso di input("")
+nome=(input("Come ti chiami? "))
+print("Ciao", nome)
+
+#Prime operazioni Aritmetiche semplici
+print('Prime operazioni Aritmetiche semplici')
+print('Somma Aritmetica semplice')
+A=input("Dammi il primo numero A: ")
+B=input("Dammi il primo numero B: ")
+somma=int(A)+int(B)
+print(f'La Somma è: ', {somma})
+
