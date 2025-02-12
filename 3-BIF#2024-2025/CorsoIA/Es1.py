ac26c75cb479b66ff1261fbccecf96a3f0c1961a
Prowler125 <massimiliamomancini@gmail.com>
2025-02-12 14:56:54 +0100

    feat: Aggiorna il primo esercizio in Python con nuove parti e variabili

--- 3-BIF#2024-2025/CorsoIA/Es1.py
+++ 3-BIF#2024-2025/CorsoIA/Es1.py
@@ -1,1 +1,13 @@
-'''Primo Esercizio in Python'''
+'''Primo Esercizio in Python del Corso I.A.'''
+#Prima Parte: uso Sintassi Base e primi tipi di Variabili
+print('Prima Parte:')
+print('Utilizzo della Sintassi Base come il "print()"')
+print('Hello World!')
+print(1, 2, 3, sep='|')
+A=10
+print(A)
+print('Seconda Parte:')
+A=10
+B=12
+C='Ciao'
+D=0,2345
