43751976de33f274747cc2a9dd406f634fa9bd00
prowler125 <massimiliamomancini@gmail.com>
2025-02-13 08:17:17 +0100

    feat: Rimuovi il file SenzaTitolo1.cpp e l'eseguibile associato, aggiungi la verifica 2° quadrimestre sui file

---
+++ 3-BIF#2024-2025/C++/Verifiche/Mancini_Massimiliano_Verifica_2_Quad_Es_A.cpp
@@ -0,0 +1,21 @@
+/*
+Verifica 2° Quadrimestre
+Informatica sui Files
+Mancini Massimiliano
+3° B-if 13/02/2025
+*/
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <math.h>
+#include <string>
+#include <string.h>
+#include <cstdlib>
+#include <ios>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
