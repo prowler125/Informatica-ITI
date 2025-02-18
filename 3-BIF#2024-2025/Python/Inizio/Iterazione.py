cce11cac6f4e2ee1e040e8247271ed23d60895b7
Prowler125 <massimiliamomancini@gmail.com>
2025-02-18 14:08:28 +0100

    feat: Aggiungi documentazione e esempi pratici per l'iterazione in Python

--- 3-BIF#2024-2025/Python/Inizio/Iterazione.py
+++ 3-BIF#2024-2025/Python/Inizio/Iterazione.py
@@ -1,4 +1,49 @@
-"""
-Parte relativa all'Iterazione
+'''
+        Terzo Esercizio in python del Corso I.A.
+        Parte relativa all'Iterazione
+'''
 
-"""
+'''
+Prima Parte: l'Iterazione e la modifica del flusso di Esecuzione
+'''
+
+#Prima Parte: primo uso delle Liste
+
+print('Prima Parte: primo uso delle Liste')
+
+#Prima Lista creata!
+numeri=[10, 20, 14]
+nomi=["Massimiliano", "Mario", "Paolo"]
+mista=[2, 'mele', 2.4, 4, 'banane', 0,99]
+
+'''Utilizziamo "len()" in Python per conoscere
+il numero degli elementi in una lista'''
+n=len(numeri)
+print("La lista contiene ", n, "elementi")
+
+
+'''
+Per estrarre e stampare un elemento
+dobbiamo conoscere la sua posizione.
+Quindi per estrarre il 1° elemento
+dalla Lista NUMERI, scriveremo:
+'''
+print(numeri[0])
+
+'''
+Questa struttura è
+composta nel seguente modo:
+
+'PRINT()'   ==      la classica istruzione per STAMPARE;
+'numeri'    ==      la Lista Numeri;
+'[0]'       ==      l'indice del 1° elemento
+'''
+
+'''
+Per stampare l'INTERO contenuto di
+una lista, scriveremo semplicemente:
+'''
+
+print(numeri)
+print(nomi)
+print(mista)
