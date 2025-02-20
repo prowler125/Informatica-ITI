7fb586dc431f7b97ef8e58c248f9d47d5bc100db
prowler125 <massimiliamomancini@gmail.com>
2025-02-20 09:48:16 +0100

    feat: Aggiungi classe Tria per calcolare proprietà dei triangoli rettangoli

--- 3-BIF#2024-2025/C++/ObjectOrientedProgrammin(OOP)/Ex.126.cpp
+++ 3-BIF#2024-2025/C++/ObjectOrientedProgrammin(OOP)/Ex.126.cpp
@@ -1,10 +1,66 @@
+/*
+Ex 126 Triangoli Rettangoli
+
+Definire una classe Tria con metodi
+off-line. Vedi sotto costruttore
+implementazione metodi off-line
+*/
+
 #include <iostream>
 #include <cmath>
 #include <math.h>
 using namespace std;
+class Tria
+{
+private:
+    int cat1, cat2;
+    double ipo, per, area;
+    string nome;
 
+public:
+    Tria(int x, int y, string z);
+    double Ipo();
+    double Per();
+    double Area();
+    string Nome();
+};
+
+Tria::Tria(int x, int y, string z)
+{
+    cat1 = x;
+    cat2 = y;
+    nome = z;
+    ipo = sqrt(x * x + y * y);
+    per = x + y + ipo;
+    area = x * y / 2;
+}
+
+double Tria::Ipo()
+{
+    return ipo;
+}
+
+double Tria::Per()
+{
+    return per;
+}
+
+double Tria::Area()
+{
+    return area;
+}
+
+string Tria::Nome()
+{
+    return nome;
+}
 
 int main()
 {
+    Tria t(3, 4, "Triangolo1");
+    cout << "Nome: " << t.Nome() << endl;
+    cout << "Ipotenusa: " << t.Ipo() << endl;
+    cout << "Perimetro: " << t.Per() << endl;
+    cout << "Area: " << t.Area() << endl;
     return 0;
 }
