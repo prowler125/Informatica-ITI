0f6df918bf6e2a8dc353d3840e4f5721dbea1636
prowler125 <massimiliamomancini@gmail.com>
2025-02-27 09:10:34 +0100

    Aggiunta della classe Tri con metodi per copia, somma e scala dei cateti

---
+++ 3-BIF#2024-2025/C++/ObjectOrientedProgrammin(OOP)/Ex.126-C.cpp
@@ -0,0 +1,7 @@
+/*
+Modificare tipo Cat1 e Cat2 in float, aggiungere i metodi seguenti:
+
+        1. Copia (Tri t)        che copia i valori del triangolo parametro nel triangolo corrente
+        2. Somma (Tri t)        che somma  i valori del triangolo parametro al triangolo corrente
+        3. Scala (int k)        che aumenta o diminuisce di k i cateti del triangolo corrente
+*/
