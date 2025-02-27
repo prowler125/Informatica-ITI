e7c06bd99d27c2a72fe77f438a486f419b3d9fb1
Massimiliano Mancini <massimiliamomancini@gmail.com>
2025-02-27 08:33:11 +0100

    Create Ex.99-F_CARICA.cpp

---
+++ 3-BIF#2024-2025/C++/FILE/Ex.99-F_CARICA.cpp
@@ -0,0 +1,4 @@
+/*ex99-F_CARICA: funzione  carica  vettore da file 
+accertarsi di avere un file vettore.txt con dei dati
+Seguendo istruzioni libro pag 225,  definire la  funzione  caricaVet, usando soliti standard   (  nomi ); 
+nel main    chiamare caricaVet  quindi  stampare il  vettore con la funzione  staVet*/
